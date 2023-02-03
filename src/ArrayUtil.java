28e4a76c741ef6ab6023ccfbb14280fd5ed48ee0
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-02-03 20:25:28 +0300

    initial commit

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ArrayUtil {
+import java.util.Arrays;
+
+public class ArrayUtil {
+    String[] arr = new String[]{"first", "second", "third", "fourth"};
+
+   public static String[] reduceEvenElement(String[] arr) {
+       String[] result = new String[arr.length / 2];
+       for (int i = 0; i < arr.length; i+=2) {
+           result[i/2] = arr[i];
+       }
+       return result;
+   }
+
+    @Override
+    public String toString() {
+        return "ArrayUtil{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
+    }
 }
